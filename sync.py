2d34f52c4c5cc738ce3c8a6cf32e8ff589287368
kiwi202518 <2410247028@mails.szu.edu.cn>
2025-10-24 17:11:47 +0800

    C:\Users\Adminstractor\-my-kaggle-data

--- sync.py
+++ sync.py
@@ -3,7 +3,7 @@ import os, shutil, subprocess
 from datetime import datetime
 
 # ===== 需修改的配置 =====
-DATA_SOURCE = "F:\CUDA\0Computer316\CuData\FiveM-MODE\15%"      # 替换为您的数据文件夹路径（如"C:/Users/Name/data"）
+DATA_SOURCE = "F:\CUDA\0Kaggle\KaggleData"      # 替换为您的数据文件夹路径（如"C:/Users/Name/data"）
 GIT_REPO = "C:\Users\Adminstractor\-my-kaggle-data"        # 替换为GitHub仓库本地路径（如"C:/my-kaggle-data"）
 # ======================
 
